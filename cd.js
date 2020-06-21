daa64a47f9d634d635dbd616dfef8574545a4ec2
unknown <17873631603@163.com>
2020-06-21 20:54:43 +0800

    d

--- cd.js
+++ cd.js
@@ -1,15 +1,8 @@
 const http = require('http')
-let child_process = require('child_process')
 
-let server = http.createServer()
+let server = http.createServer((req, res) => {
+    console.log("callback")
+    res.end("hello")
+});
 
-server.on('request', function (req, resp) {
-    console.log("接到一次推送")
-    child_process.exec('./ci.sh', function (error, stdout, stderror) {
-        console.log(stdout)
-    })
-    resp.end("receive one push")
-})
-server.listen(3000, function () {
-    console.log("持续集成服务器已在3000端口启动")
-})
+server.listen(3000)
