7b9953a61ce4497d4873d800a72409dc8785fa24
unknown <17873631603@163.com>
2020-06-21 18:47:58 +0800

    first

---
+++ src/main/java/cn/simon/crm/controller/CommenController.java
@@ -0,0 +1,23 @@
+package cn.simon.crm.controller;
+
+import cn.simon.crm.pojo.Album;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class CommenController {
+    @RequestMapping(value = "/albums")
+    public List<Album> findAllAlbums(){
+        List albums = new ArrayList();
+        for (int i = 0;i < 3;i++){
+            Album a = new Album();
+            a.setId(i);
+            a.setName("album"+i);
+            albums.add(a);
+        }
+        return albums;
+    }
+}
